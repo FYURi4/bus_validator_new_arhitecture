02a6a53a283ac69e3d8397dd1f2c2d4b80bec7fb
FURUSHKA <163753122+FYURi4@users.noreply.github.com>
2026-05-19 11:03:56 +0300

    Update shared.lua

---
+++ lua/entities/bm20/shared.lua
@@ -0,0 +1,21 @@
+ENT.Base = "base_gmodentity"
+
+ENT.Type = "anim"
+
+ENT.Category = "3DEMC_ENT"
+
+ENT.Spawnable = true
+
+ENT.AdminSpawnable = true
+
+ENT.ClassName = "bm20"
+
+ENT.PrintName = "bm20"
+
+ENT.Author = "FURA"
+
+ENT.Contact = "Discord: fyurl4, Furushka: FurichF"
+
+ENT.Purpose = "Создание блинчиков."
+
+ENT.Instructions = "Подойти к модели, нажать 'E'. "
